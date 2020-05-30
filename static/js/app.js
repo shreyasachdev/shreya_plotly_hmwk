51232a02beb5c6d1ba7cede8ceba25512debda31
shreyasachdev <60080730+shreyasachdev@users.noreply.github.com>
2020-05-30 01:07:42 -0400

    4

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,8 @@
 // Use d3.json() to fetch data from JSON file
 // Incoming data is internally referred to as incomingData
-var data = d3.json("samples.json") function(data){
-    console.log(data)};
+var data = d3.json("samples.json", function(sampledata) {
+    console.log(sampledata)
+});
 // var values = data.sample_values
 // var labels = data.otu_ids
 // var hovertext = data.otu_labels
